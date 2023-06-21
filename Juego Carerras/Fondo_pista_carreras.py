81e8c49be2b19950baa66da3835248faa8a28dc5
PaulJonatha <p.ramirez9991@gmail.com>
2023-06-21 14:40:07 -0300

    testeando

--- Juego Carerras/Fondo_pista_carreras.py
+++ Juego Carerras/Fondo_pista_carreras.py
@@ -17,14 +17,5 @@ class PistaCarreras(CargarImagen):
         self.pantalla.blit(self.imagen, (0, self.posicion_y))
         self.pantalla.blit(self.imagen, (0, self.posicion_y - ALTO_VENTANA))
 #------------------------------------------------------------------------------------------
-    """    def dibujar_pista(self):
-            altura_fondo = self.imagen.get_rect().height
-            y_relativa = self.posicion_y % altura_fondo
-            posicion_inicial = (0, y_relativa - altura_fondo)
-            self.pantalla.blit(self.imagen, posicion_inicial)
-            if y_relativa < ALTO_VENTANA:
-                posicion_segunda = (0, y_relativa)
-                self.pantalla.blit(self.imagen, posicion_segunda)
-            self.posicion_y += 11"""
     
 
